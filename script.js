70d6f714b2b25628049ac94cfa58503b5356f79b
Dhanashree <dhanashreeakharade@gmail.com>
2026-01-12 02:29:29 -0500

    task 1 completed

---
+++ script.js
@@ -0,0 +1,16 @@
+
+
+function formatFullName(firstName,lastName){
+    let fullName = lastName + " " + firstName;
+    return fullName;
+
+}
+
+function capitalizeFirstLetter(firstName,lastName){
+    return String(lastName).charAt(0).toUpperCase()+String
+}
+
+ let fullName = formatFullName('tony','stark');
+console.log(fullName);
+
+
